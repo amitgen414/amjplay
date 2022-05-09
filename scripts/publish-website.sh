82569da1f3c23aac559e5a77eab7128dbecd8400
Amit Jha <amitkumarjha@fico.com>
2022-05-09 18:30:21 +0200

    updates deployment scripts

--- scripts/publish-website.sh
+++ scripts/publish-website.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 yarn build
-aws s3 rm s3://amjplay.com --recursive
-aws s3 sync dist s3://amjplay.com/
+aws s3 sync dist s3://amjplay.com/ \
+--delete \
+--exclude index.html \
+--cache-control max-age=31536000,public,immutable
+
+
+aws s3 sync dist s3://amjplay.com/ \
+--delete \
+--exclude "*!index.html" \
+--cache-control max-age=0,no-cache,no-store,must-revalidate
